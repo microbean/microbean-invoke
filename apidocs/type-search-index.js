665cb335a803e6ad846e9ba9c658daf9b79f613e
Laird Nelson <ljnelson@gmail.com>
2022-02-12 19:47:06 +0000

    Building Github Pages site for microbean-invoke 0.0.3

--- apidocs/type-search-index.js
+++ apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.microbean.invoke","l":"BootstrapMethods"},{"p":"org.microbean.invoke","l":"CachingSupplier"},{"p":"org.microbean.invoke","l":"ConstantDescs"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.microbean.invoke","l":"BootstrapMethods"},{"p":"org.microbean.invoke","l":"CachingSupplier"},{"p":"org.microbean.invoke","l":"ConstantDescs"},{"p":"org.microbean.invoke","l":"OptionalSupplier"}];updateSearchResults();
